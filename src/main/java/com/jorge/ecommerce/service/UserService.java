d58324e2a78cdb6eb7c7a593727d1d11d488c562
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-11 17:58:55 -0500

    Add UserService

---
+++ src/main/java/com/jorge/ecommerce/service/UserService.java
@@ -0,0 +1,45 @@
+package com.jorge.ecommerce.service;
+
+import com.jorge.ecommerce.dto.UserDto;
+import com.jorge.ecommerce.model.User;
+import com.jorge.ecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public List<UserDto> findAll() {
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtos = new ArrayList<>();
+        users.forEach(user -> userDtos.add(
+                modelMapper.map(user, UserDto.class)
+        ));
+        return userDtos;
+    }
+
+    public UserDto findById(Long id) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        return modelMapper.map(existingUser, UserDto.class);
+    }
+
+    public UserDto save(UserDto userDto) {
+        User user = modelMapper.map(userDto, User.class);
+        user = userRepository.save(user);
+        return modelMapper.map(user, UserDto.class);
+    }
+
+    public UserDto update(Long id, UserDto userDto) {
+        User user = userRepository.findById(id).orElse(null);
+        modelMapper.map(userDto, user);
+        user = userRepository.save(user);
+        return modelMapper.map(user, UserDto.class);
+    }
+}
