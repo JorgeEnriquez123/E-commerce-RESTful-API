0b31d7a01be409d435ae556f0effb9d868d628f3
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-14 13:19:51 -0500

    Add Category Entity

---
+++ src/main/java/com/jorge/ecommerce/model/Category.java
@@ -0,0 +1,20 @@
+package com.jorge.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String name;
+}
