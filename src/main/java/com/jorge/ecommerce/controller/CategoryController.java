bc480717897599138be2555b489240c7efcf2b95
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-15 13:02:20 -0500

    Add Category Controller

---
+++ src/main/java/com/jorge/ecommerce/controller/CategoryController.java
@@ -0,0 +1,38 @@
+package com.jorge.ecommerce.controller;
+
+import com.jorge.ecommerce.dto.CategoryDto;
+import com.jorge.ecommerce.dto.create.CreateCategoryDto;
+import com.jorge.ecommerce.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getCategoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CreateCategoryDto categoryDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.save(categoryDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDto> updateCategory(@PathVariable Long id, @RequestBody CreateCategoryDto categoryDto) {
+        return ResponseEntity.ok(categoryService.update(id, categoryDto));
+    }
+}
