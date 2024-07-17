684410bce9e7bce44c056a3ed58391b3da9ea4d4
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-16 22:02:56 -0500

    Add HttpMessageNotReadableException handler

--- src/main/java/com/jorge/ecommerce/handlers/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jorge/ecommerce/handlers/exception/GlobalExceptionHandler.java
@@ -1,18 +1,33 @@
 package com.jorge.ecommerce.handlers.exception;
 
 import com.jorge.ecommerce.handlers.response.ErrorResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), "Invalid JSON");
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse ValidationExceptionHadler(MethodArgumentNotValidException ex) {
