f9819eeb46d01e04db4a266e26e56a2db80438c2
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-23 13:02:11 -0500

    Add Order and OrderDetails entities

---
+++ src/main/java/com/jorge/ecommerce/model/Order.java
@@ -0,0 +1,35 @@
+package com.jorge.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal total;
+    @Column(nullable = false)
+    private String status;
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shipping_address_id", nullable = false)
+    private AddressLine shippingAddress;
+}
--- src/main/java/com/jorge/ecommerce/model/OrderDetail.java
+++ src/main/java/com/jorge/ecommerce/model/OrderDetail.java
@@ -1,24 +1,28 @@
 package com.jorge.ecommerce.model;
 
-import com.jorge.ecommerce.dto.CartDto;
-import com.jorge.ecommerce.dto.UserDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Cart {
+public class OrderDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
 }
