c2ce429cfa32b1afeea0af7c72d749429ed6824c
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-26 01:53:42 -0500

    Add JwtFilter and SecurityManager

---
+++ src/main/java/com/jorge/ecommerce/configuration/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.jorge.ecommerce.configuration.filter;
+
+import com.jorge.ecommerce.configuration.security.SecurityManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final SecurityManager securityManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var jwtToken = getTokenFromRequest(request);
+        if(jwtToken != null) {
+            securityManager.authenticate(jwtToken)
+                    .ifPresent(SecurityContextHolder.getContext()::setAuthentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/jorge/ecommerce/configuration/security/SecurityManager.java
@@ -0,0 +1,46 @@
+package com.jorge.ecommerce.configuration.security;
+
+import com.jorge.ecommerce.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityManager {
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    public Optional<Authentication> authenticate(String jwt){
+        try {
+            var username = jwtUtil.extractUsername(jwt);
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                var userAuthenticated = userDetailsService.loadUserByUsername(username);
+                if(jwtUtil.isTokenValid(jwt, userAuthenticated)){
+                    return of(new UsernamePasswordAuthenticationToken(
+                            userAuthenticated,
+                            null,
+                            new ArrayList<>()));
+                }
+            }
+        }
+        catch (UsernameNotFoundException ex) {
+            log.error("User not found: {}", ex.getMessage());
+        } catch (Exception ex){
+            log.error("JWT token could not be validated. {}", ex.getMessage());
+        }
+        return empty();
+    }
+}
