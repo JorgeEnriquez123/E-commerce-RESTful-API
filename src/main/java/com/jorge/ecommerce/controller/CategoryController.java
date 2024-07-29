2769cdbf398da5ad0dfcc308c2a31c1a3941d16f
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-07-28 20:52:58 -0500

    Change base RequestMapping in CategoryController

--- src/main/java/com/jorge/ecommerce/controller/CategoryController.java
+++ src/main/java/com/jorge/ecommerce/controller/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
