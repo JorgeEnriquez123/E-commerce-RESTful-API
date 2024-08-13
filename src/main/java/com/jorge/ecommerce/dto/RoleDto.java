d3f1d950cc0de2867ca18a8805a733df244cff94
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-08-13 01:08:38 -0500

    Add RoleDto

--- src/main/java/com/jorge/ecommerce/dto/RoleDto.java
+++ src/main/java/com/jorge/ecommerce/dto/RoleDto.java
@@ -1,21 +1,15 @@
 package com.jorge.ecommerce.dto;
 
-import com.jorge.ecommerce.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UserDto {
+public class RoleDto {
     private Long id;
-    private String username;
-    private String firstName;
-    private String lastName;
-    private List<RoleDto> roles;
+    private String name;
 }
