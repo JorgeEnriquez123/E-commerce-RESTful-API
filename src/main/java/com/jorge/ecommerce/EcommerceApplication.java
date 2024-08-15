200a12ab7a597a30a5d133c872d3a0827fef4444
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-08-15 01:25:23 -0500

    Add User init and User Controller testing

--- src/main/java/com/jorge/ecommerce/EcommerceApplication.java
+++ src/main/java/com/jorge/ecommerce/EcommerceApplication.java
@@ -1,17 +1,23 @@
 package com.jorge.ecommerce;
 
+import com.jorge.ecommerce.dto.create.CreateUserDto;
 import com.jorge.ecommerce.model.Category;
 import com.jorge.ecommerce.model.Product;
 import com.jorge.ecommerce.model.Role;
+import com.jorge.ecommerce.model.User;
 import com.jorge.ecommerce.repository.CategoryRepository;
 import com.jorge.ecommerce.repository.ProductRepository;
 import com.jorge.ecommerce.repository.RoleRepository;
+import com.jorge.ecommerce.repository.UserRepository;
+import com.jorge.ecommerce.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -22,60 +28,80 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableCaching
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
-@RequiredArgsConstructor
-public class EcommerceApplication implements CommandLineRunner {
-	private final CategoryRepository categoryRepository;
-	private final ProductRepository productRepository;
-	private final RoleRepository roleRepository;
-
+public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Category category = Category.builder().name("Phones").build();
-		Category savedCategory = categoryRepository.save(category);
+	@Bean
+	public CommandLineRunner init(CategoryRepository categoryRepository,
+								  ProductRepository productRepository,
+								  RoleRepository roleRepository,
+								  AuthService authService
+	) {
+		return args -> {
+			Category category = Category.builder().name("Phones").build();
+			Category savedCategory = categoryRepository.save(category);
 
-		List<Product> products = Arrays.asList(
-				Product.builder()
-						.name("iPhone 15")
-						.price(BigDecimal.valueOf(3200.00))
-						.category(savedCategory)
-						.stockQuantity(5)
-						.build(),
-				Product.builder()
-						.name("Samsung Galaxy S24")
-						.price(BigDecimal.valueOf(3100.00))
-						.category(savedCategory)
-						.stockQuantity(20)
-						.build(),
-				Product.builder()
-						.name("Xiaomi 14 Ultra")
-						.price(BigDecimal.valueOf(4200.00))
-						.category(savedCategory)
-						.stockQuantity(17)
-						.build(),
-				Product.builder()
-						.name("Huawei Pura 70 Pro")
-						.price(BigDecimal.valueOf(4400.00))
-						.category(savedCategory)
-						.stockQuantity(8)
-						.build(),
-				Product.builder()
-						.name("Google Pixel 8 pro")
-						.price(BigDecimal.valueOf(4600.00))
-						.category(savedCategory)
-						.stockQuantity(12)
-						.build()
-		);
+			List<Product> products = Arrays.asList(
+					Product.builder()
+							.name("iPhone 15")
+							.price(BigDecimal.valueOf(3200.00))
+							.category(savedCategory)
+							.stockQuantity(5)
+							.build(),
+					Product.builder()
+							.name("Samsung Galaxy S24")
+							.price(BigDecimal.valueOf(3100.00))
+							.category(savedCategory)
+							.stockQuantity(20)
+							.build(),
+					Product.builder()
+							.name("Xiaomi 14 Ultra")
+							.price(BigDecimal.valueOf(4200.00))
+							.category(savedCategory)
+							.stockQuantity(17)
+							.build(),
+					Product.builder()
+							.name("Huawei Pura 70 Pro")
+							.price(BigDecimal.valueOf(4400.00))
+							.category(savedCategory)
+							.stockQuantity(8)
+							.build(),
+					Product.builder()
+							.name("Google Pixel 8 pro")
+							.price(BigDecimal.valueOf(4600.00))
+							.category(savedCategory)
+							.stockQuantity(12)
+							.build()
+			);
 
-		productRepository.saveAll(products);
+			productRepository.saveAll(products);
 
-		Role role = Role.builder().name("CUSTOMER").build();
-		roleRepository.save(role);
+			Role role = Role.builder().name("CUSTOMER").build();
+			roleRepository.save(role);
 
-		Role role1 = Role.builder().name("ADMIN").build();
-		roleRepository.save(role1);
+			Role role1 = Role.builder().name("ADMIN").build();
+			roleRepository.save(role1);
+
+			CreateUserDto user = CreateUserDto.builder()
+					.firstName("Test")
+					.lastName("Subject")
+					.username("Test111")
+					.password("password")
+					.role("ADMIN")
+					.build();
+			authService.register(user);
+
+			CreateUserDto user2 = CreateUserDto.builder()
+					.firstName("Test 2")
+					.lastName("Subject 2")
+					.username("Test222")
+					.password("password")
+					.role("CUSTOMER")
+					.build();
+
+			authService.register(user2);
+		};
 	}
 }
---
+++ src/test/java/com/jorge/ecommerce/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,99 @@
+package com.jorge.ecommerce.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jorge.ecommerce.dto.auth.AddRoleToUserDto;
+import com.jorge.ecommerce.dto.create.CreateUserDto;
+import com.jorge.ecommerce.dto.update.UpdateUserDto;
+import com.jorge.ecommerce.service.AuthService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    //Needed for endpoints with @AuthenticationPrincipal parameters
+    @BeforeEach
+    void setUpSecurityContext(){
+        var user = userDetailsService.loadUserByUsername("Test111");
+        System.out.println(user);
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                    user, null, user.getAuthorities()
+                )
+        );
+    }
+
+    // Test updateUserPersonalInfo for a logged-in user
+    @Test
+    //@WithMockUser(username = "Test123", roles = {"CUSTOMER"})
+    public void testUpdateUserPersonalInfo() throws Exception {
+        UpdateUserDto updateUserDto = UpdateUserDto.builder()
+                .firstName("updatedFirstName")
+                .lastName("updatedLastName")
+                .username("Test123")
+                .password("newPassword")
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateUserDto)))
+                .andExpect(status().isOk());
+    }
+
+    // Test getAll for admin
+    @Test
+    public void testGetAllUsers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andExpect(status().isOk());
+    }
+
+    // Test getUserById for admin
+    @Test
+    public void testGetUserById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/2"))
+                .andExpect(status().isOk());
+    }
+
+    // Test addRoleToUser for admin
+    @Test
+    public void testAddRoleToUser() throws Exception {
+        AddRoleToUserDto addRoleToUserDto = new AddRoleToUserDto();
+        addRoleToUserDto.setRole("ADMIN");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/users/2/roles")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(addRoleToUserDto)))
+                .andExpect(status().isNoContent());
+    }
+
+    // Test deleteRoleFromUser for admin
+    @Test
+    public void testDeleteRoleFromUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/users/2/roles/1"))
+                .andExpect(status().isNoContent());
+    }
+}
